5c87dc71b6c122d7d3456fc2068e0ff50fe7f789
Tecinco <Tecinco@MacBook-Pro-de-Tecinco.local>
2018-03-01 16:34:28 -0300

    primer commit

---
+++ src/app/pages/grraficas1/grraficas1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grraficas1',
+  templateUrl: './grraficas1.component.html',
+  styles: []
+})
+export class Grraficas1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
